a71869ab47a722b367db10adbf965f095bf041ae
Marco Fontani <MFONTANI@cpan.org>
2016-11-03 14:58:30 +0000

    perl5: add local::lib on ~/perl5/lib/perl5

--- custom/999-dev-perl5-lib.zsh
+++ custom/999-dev-perl5-lib.zsh
@@ -2,3 +2,7 @@
 if [[ "$HOSTNAME" == "dev1.theregister.co.uk" ]]; then
     export PERL5LIB=lib:/reg/repo/trois/lib:$PERL5LIB
 fi
+
+if [[ -d ~/perl5/lib/perl5 ]]; then
+    eval $( perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+fi
