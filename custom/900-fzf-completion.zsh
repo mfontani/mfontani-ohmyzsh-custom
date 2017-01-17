00f922b987fd41a213207b71be12daba0765dba1
Marco Fontani <MFONTANI@cpan.org>
2017-01-17 10:19:09 +0100

    ensure all space is used for fzf completion

--- custom/900-fzf-completion.zsh
+++ custom/900-fzf-completion.zsh
@@ -3,7 +3,7 @@ export FZF_COMPLETION_TRIGGER='~~'
 
 # supports patterns like ^… or …$ or !… or 'foo (exactly matches "foo")
 export FZF_COMPLETION_OPTS='-x'
-export FZF_DEFAULT_OPTS='-x'
+export FZF_DEFAULT_OPTS='-x --height=100%'
 
 # FZF https://github.com/junegunn/fzf
 if [ -f ~/.fzf.zsh ]; then
