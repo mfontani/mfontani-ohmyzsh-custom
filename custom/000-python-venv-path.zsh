adec9d046c369cd4f69e1260a0dc0d8da9bc35a4
Marco Fontani <MFONTANI@cpan.org>
2017-05-02 16:56:23 +0200

    [custom] rm additional python exports
    ... as brew started throwing a tantrum or two, and it's not like I'm
    using them much anyway

--- custom/000-python-venv-path.zsh
+++ custom/000-python-venv-path.zsh
@@ -1,5 +1,6 @@
-export PATH=$PATH:~/python/venv/bin/
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
-export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig/:$PKG_CONFIG_PATH"
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
-export PATH="/Users/marco/Library/Python/2.7/bin:$PATH"
+# export PATH=$PATH:~/python/venv/bin/
+# export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+# export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig/:$PKG_CONFIG_PATH"
+# export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+# export PATH="/Users/marco/Library/Python/2.7/bin:$PATH"
+unset PYTHONPATH
