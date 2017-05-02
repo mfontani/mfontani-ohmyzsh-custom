2a2a56e0c3af1e8c599017376eb01b025ae09070
Marco Fontani <MFONTANI@cpan.org>
2017-05-02 16:57:12 +0200

    [custom] make EDITOR=nvim

--- custom/200-vim-as-editor.zsh
+++ custom/200-vim-as-editor.zsh
@@ -1,9 +1,9 @@
-export EDITOR=vim
+export EDITOR=nvim
 
-if [[ -x /usr/local/bin/brew ]]; then
-    export EDITOR=$(brew ls macvim | grep Contents/MacOS/Vim)
-    alias vim=$(brew ls macvim | grep Contents/MacOS/Vim)
-fi
+# if [[ -x /usr/local/bin/brew ]]; then
+#     export EDITOR=$(brew ls macvim | grep Contents/MacOS/Vim)
+#     alias vim=$(brew ls macvim | grep Contents/MacOS/Vim)
+# fi
 
 # Allow me to use C-s to save in Vim
 stty stop '' -ixoff
