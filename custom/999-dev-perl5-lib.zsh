f1b8262ce89b595ed619d63c091191ee1471b1af
Marco Fontani <MFONTANI@cpan.org>
2017-07-20 16:57:05 +0100

    perl5: stop exporting $work lib include
    ... as that has caused me more issues than it has ever fixed.

--- custom/999-dev-perl5-lib.zsh
+++ custom/999-dev-perl5-lib.zsh
@@ -1,7 +1,7 @@
 # On dev, include repo's lib
-if [[ "$HOSTNAME" == "dev1.theregister.co.uk" ]]; then
-    export PERL5LIB=lib:/reg/repo/trois/lib:$PERL5LIB
-fi
+# if [[ "$HOSTNAME" == "dev1.theregister.co.uk" ]]; then
+#     export PERL5LIB=lib:/reg/repo/trois/lib:$PERL5LIB
+# fi
 
 if [[ -d ~/perl5/lib/perl5 ]]; then
     eval $( perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
