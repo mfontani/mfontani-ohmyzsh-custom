e3a9dd40ba5adf980288892965f05861e60bc45f
Marco Fontani <MFONTANI@cpan.org>
2017-11-07 02:34:56 +0100

    [custom] fix for fd

--- custom/920-fzf-default-command.zsh
+++ custom/920-fzf-default-command.zsh
@@ -20,14 +20,10 @@ fi
 # If we have "fd" - https://github.com/sharkdp/fd - use it
 if [[ -x /usr/local/bin/fd || -x ~/bin/fd ]]; then
     export FZF_DEFAULT_COMMAND='fd --hidden --type f'
-else
-    # The following function overrides will only work if we have fd.
-    exit 0
+    _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
+    }
+    _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
 fi
-
-_fzf_compgen_path() {
-    fd --hidden --follow --exclude ".git" . "$1"
-}
-_fzf_compgen_dir() {
-    fd --type d --hidden --follow --exclude ".git" . "$1"
-}
