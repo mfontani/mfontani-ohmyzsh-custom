15fef57ea6d88bd6e7163e914adadfc4162cded3
Marco Fontani <MFONTANI@cpan.org>
2017-11-08 13:46:04 +0000

    [custom] export COLORTERM when connecting via VMs

--- custom/500-exports.zsh
+++ custom/500-exports.zsh
@@ -5,3 +5,7 @@ export TERM=xterm-256color
 if [[ $SSH_CLIENT =~ '^172[.]16[.]200[.]1[ ]' ]]; then
     export COLORTERM=truecolor
 fi
+# Same goes if I'm connecting via ssh from the macbook's VM to another vm:
+if [[ $SSH_CLIENT =~ '^10[.]0[.]2[.]2[ ]' ]]; then
+    export COLORTERM=truecolor
+fi
