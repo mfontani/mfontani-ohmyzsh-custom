e528ba0fea3a58c9963007136b9b49bb81795329
Marco Fontani <MFONTANI@cpan.org>
2017-11-08 09:50:39 +0000

    [custom] export COLORTERM=truecolor when SSHing in
    ... from my mac into a Linux VBox

--- custom/500-exports.zsh
+++ custom/500-exports.zsh
@@ -1,1 +1,7 @@
 export TERM=xterm-256color
+
+# If I'm connecting via ssh from the macbook, using iTerm2, which I know comes
+# with $COLORTERM=truecolor, set it inside my dev environments too:
+if [[ $SSH_CLIENT =~ '^172[.]16[.]200[.]1[ ]' ]]; then
+    export COLORTERM=truecolor
+fi
